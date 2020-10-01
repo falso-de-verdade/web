9789756f02951c59032184a6e5d5bf184b6b39d9
Yan Marques de Cerqueira <yan.cerqueira@unisul.br>
2020-10-01 15:50:48 -0300

    Shows condominium of resident invite

--- src/views/resident/ResidentRegistration.jsx
+++ src/views/resident/ResidentRegistration.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+import { Col } from "reactstrap";
+import { Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import Subscriber from '../../assets/img/subscriber.svg'
 import SignUpComponent from "components/SignUp/SignUpComponent";
+import { TableComponent } from "components/Listing";
+import Button from "components/CustomButton/CustomButton.jsx";
 
 const onManagerFromAccount = (user, history) => {
   console.log(user);
@@ -12,14 +17,73 @@ const onRegister = (name, email, password, history) => {
   console.log(name, email, password);
 }
 
+const condominiumHeaders = [
+  "Nome",
+  "Endereço",
+]
+
+const condominiumDataReducer = condominium => [
+  condominium.name,
+  condominium.address,
+]
+
+const targetCondominium = {
+  name: "Condomínio Flores Flatulentas",
+  address: "Avenida Não Existe, Nº 0"
+}
+
 const ResidentRegistration = props => {
-  return (
-    <SignUpComponent 
-        onUserFromAccount={onManagerFromAccount} 
-        onRegister={onRegister}
-        image={Subscriber}
-        />
-  );
+  const [showCondoModal, setShowCondoModal] = React.useState(true);
+
+  const hideCondoModal = () => setShowCondoModal(false);
+
+  return <React.Fragment>
+      <SignUpComponent 
+          onUserFromAccount={onManagerFromAccount} 
+          onRegister={onRegister}
+          image={Subscriber}
+          />
+
+      <Modal
+        show={showCondoModal}
+        aria-labelledby="contained-modal-title">
+        <Modal.Header>
+          <Modal.Title>Condomínio</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p className="text-center">
+            Este é o condomínio apresentado pelo convite.
+          </p>
+          <p className="text-center">
+            Por favor confirme se é o condomínio desejado.
+          </p>
+          <TableComponent 
+            items={[targetCondominium]}
+            headers={condominiumHeaders}
+            dataReducer={condominiumDataReducer}
+            />
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Col md={2}>
+            <Button 
+              bsStyle="success" 
+              onClick={hideCondoModal}
+              fill
+              pullLeft>
+              Esse é meu condomínio!
+            </Button>{' '}
+          </Col>
+
+          <Link to={"/support"}>
+            <Button bsStyle="danger" fill>
+              Não é meu condomínio
+            </Button>{' '}
+          </Link>
+        </Modal.Footer>
+      </Modal>
+    </React.Fragment>
 }
 
 export default ResidentRegistration;
