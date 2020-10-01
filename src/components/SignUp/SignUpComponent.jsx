c5e1834ae8a88e33932c3c88de9b04339d684c60
Yan Marques de Cerqueira <yan.cerqueira@unisul.br>
2020-10-01 02:46:38 -0300

    Add button to use an existing account

---
+++ src/components/SignUp/SignUpComponent.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+import { useHistory } from "react-router-dom";
+import { AvForm } from 'availity-reactstrap-validation';
+import { Modal } from "react-bootstrap";
+
+import SignInComponent from "components/SignIn/SignInComponent";
+import Button from "components/CustomButton/CustomButton.jsx";
+import InputCustom from '../../components/inputs/inputCustom';
+
+const styleContent = {
+  width: '100%',
+  maxWidth: '1120px',
+  height: '100vh',
+  margin: '0 auto',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+};
+
+const handleLogin = (email, password) => {
+    return {
+        name: "Usuário"
+    }
+}
+
+const SignUpComponent = props => {
+  const [showSignInModal, setShowSignInModal] = React.useState(false);
+
+  const history = useHistory();
+
+  const onInternalSubmit = (email, password) => {
+      const user = handleLogin(email, password);
+
+      if (user !== false) {
+          props.onUserFromAccount(user, history);
+      }
+  }
+
+  return (
+    <div className="content" style={styleContent}>
+      <Col md={6}>
+        <div style={{ padding: '16px', boxShadow: '0 0 100px rgba(21, 50, 90, 0.7)', backgroundColor: ' #4091ff', borderRadius: '6px' }}>
+          <AvForm autoComplete="off" onSubmit={e => props.onRegister("Yan", "teste@teste", "1234", history)}>
+            <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
+              <Col md={12} style={{ marginTop: '-10px' }}>
+                <InputCustom
+                  id="name"
+                  name="name"
+                  type="text"
+                  descricao="Nome completo" />
+              </Col>
+              <Col md={12} style={{ marginTop: '-10px' }}>
+                <InputCustom
+                  id="email"
+                  name="email"
+                  type="email"
+                  descricao="E-mail" />
+              </Col>
+            </Row>
+            <Row style={{ paddingLeft: '10px', paddingRight: '10px' }}>
+              <Col md={12} style={{ marginTop: '-10px' }}>
+                <InputCustom
+                  id="password"
+                  name="password"
+                  type="password"
+                  descricao="Senha" />
+              </Col>
+              <Col md={12} style={{ marginTop: '-10px' }}>
+                <InputCustom
+                  id="confirmedPassword"
+                  name="confirmedPassword"
+                  type="password"
+                  descricao="Confirma senha" />
+              </Col>
+            </Row>
+            <Row style={{ padding: '15px' }}>
+              <Col md={2}>
+                <Button type="submit" bsStyle="success" fill>Salvar</Button>
+              </Col>
+              {props.operations && props.operations}
+            </Row>
+            <Row style={{ padding: '15px' }}>
+              <Col md={8}>
+                <Button bsStyle="info" fill pullRight
+                  onClick={() => setShowSignInModal(true)}>
+                    Usar conta existente
+                </Button>
+              </Col>
+            </Row>
+          </AvForm>
+        </div>
+      </Col>
+
+        <Modal
+            show={showSignInModal}
+            aria-labelledby="contained-modal-title">
+            <SignInComponent 
+                onSubmit={onInternalSubmit} 
+                operations={
+                    <Button
+                        fill 
+                        pullRight 
+                        bsStyle="danger" 
+                        onClick={() => setShowSignInModal(false)}>
+                        Cancelar
+                    </Button>
+                } />
+        </Modal>
+
+      <img src={props.image} />
+    </div>
+  );
+}
+
+export default SignUpComponent;
--- src/views/manager/ManagerRegistration.jsx
+++ src/views/manager/ManagerRegistration.jsx
@@ -1,82 +1,39 @@
 import React, { Component } from "react";
-import Button from "components/CustomButton/CustomButton.jsx";
-import { Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Col } from "reactstrap";
+import { Link, useHistory } from "react-router-dom";
 
-import { AvForm } from 'availity-reactstrap-validation';
-import InputCustom from '../../components/inputs/inputCustom';
 import Subscriber from '../../assets/img/subscriber.svg'
+import Button from "components/CustomButton/CustomButton.jsx";
+import SignUpComponent from "components/SignUp/SignUpComponent";
 
-class ManagerRegistration extends Component {
+const onResidentFromAccount = (user, history) => {
+  console.log(user);
+  // history.push("/admin/dashboard");
+}
 
-  render() {
+const onRegister = (name, email, password, history) => {
+  console.log(name, email, password);
+}
 
-    const styleContent = {
-      width: '100%',
-      maxWidth: '1120px',
-      height: '100vh',
-      margin: '0 auto',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between'
-    };
+const BackOperations = props => {
+  return (
+    <Col md={10}>
+      <Link to="/login">
+        <Button bsStyle="warning" fill pullRight>Voltar</Button>
+      </Link>
+    </Col>
+  )
+}
 
-    return (
-      <div className="content" style={styleContent}>
-        <Col md={6}>
-          <div style={{ padding: '16px', boxShadow: '0 0 100px rgba(21, 50, 90, 0.7)', backgroundColor: ' #4091ff', borderRadius: '6px' }}>
-            <AvForm autoComplete="off" onSubmit={this.operacaoCliente} ref="formCliente">
-              <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
-                <Col md={12} style={{ marginTop: '-10px' }}>
-                  <InputCustom
-                    id="name"
-                    name="name"
-                    type="text"
-                    descricao="Nome completo" />
-                </Col>
-                <Col md={12} style={{ marginTop: '-10px' }}>
-                  <InputCustom
-                    id="email"
-                    name="email"
-                    type="email"
-                    descricao="E-mail" />
-                </Col>
-              </Row>
-              <Row style={{ paddingLeft: '10px', paddingRight: '10px' }}>
-                <Col md={12} style={{ marginTop: '-10px' }}>
-                  <InputCustom
-                    id="password"
-                    name="password"
-                    type="password"
-                    descricao="Senha" />
-                </Col>
-                <Col md={12} style={{ marginTop: '-10px' }}>
-                  <InputCustom
-                    id="confirmedPassword"
-                    name="confirmedPassword"
-                    type="password"
-                    descricao="Confirma senha" />
-                </Col>
-              </Row>
-              <Row style={{ padding: '15px' }}>
-                <Col md={6}>
-                  <Link to="/login">
-                    <Button bsStyle="warning" fill>Voltar</Button>
-                  </Link>
-                </Col>
-                <Col md={6}>
-                  <Button type="submit" bsStyle="success" fill pullRight>Salvar</Button>
-                </Col>
-              </Row>
-            </AvForm>
-          </div>
-        </Col>
-
-        {/* TODO change a nice picture */}
-        <img src={Subscriber} alt="Inscreva-se" />
-      </div>
-    );
-  };
-};
+const ManagerRegistration = props => {
+  return (
+    <SignUpComponent 
+      onUserFromAccount={onResidentFromAccount} 
+      onRegister={onRegister}
+      image={Subscriber}
+      operations={<BackOperations />}
+      />
+  );
+}
 
 export default ManagerRegistration;
