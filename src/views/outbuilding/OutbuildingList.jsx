cce33e0445d8181ceb766c3a218c2641936f8eb2
Developer <developer0@fedora-dev.optimus-prime.homelab>
2020-11-22 22:27:55 -0300

    Implement outbuilding listing integration

--- src/views/outbuilding/OutbuildingList.jsx
+++ src/views/outbuilding/OutbuildingList.jsx
@@ -1,181 +1,85 @@
-import React, { Component } from "react";
-
-import { Modal, Table, FormControl } from "react-bootstrap";
-import { Row, Col, Button } from "reactstrap";
-
-import Card from "components/Card/Card";
-
-import { AvForm, AvField } from 'availity-reactstrap-validation';
-import ButtonB from "components/CustomButton/CustomButton.jsx";
-
-import api from '../../services/api';
+import React from "react";
 import { Link } from "react-router-dom";
+
+import ButtonB from "components/CustomButton/CustomButton.jsx";
 import { UserAuthContext } from "contexts";
-import * as qs from 'query-string';
+import { Listing } from "components/Listing";
+import OutbuildingDomain from "domains/outbuilding";
 
-class OutbuildingList extends Component {
+const dataReducer = outb => [
+  outb.name,
+  outb.capacity,
+]
 
-  render() {
+const Headers = [
+  "Nome", 
+  // "Disponibilidade", 
+  "Capacidade", 
+  // "Local",
+]
 
-    return (
-      <div className="content">
-
-        <Row style={{ marginTop: '-20px' }}>
-          <Col md={4} style={{ marginBottom: '5px', paddingRight: 10 }}>
-            <FormControl
-              type="text"
-              //value={this.state.inputLocalizar}
-              onChange={(e) => { this.setState({ inputLocalizar: e.target.value }) }}
-              onKeyUp={(valor) => valor.key == 'Enter' ? this.localizarDependencias() : ''}
-              placeholder="Localizar Dependência"
-            />
-          </Col>
-
-          <Col md={4} style={{ marginBottom: '5px', paddingLeft: 0 }}>
-            <ButtonB bsStyle="info"
-              //disabled={this.state.disabledButtons}
-              onClick={this.listarProdutos}>
-              <span className="fa fa-search"></span>
-              Localizar
-              </ButtonB>
-
-            <UserAuthContext.Consumer>
-              {user => {
-                if (user.isManager) {
-                  return (
-                    <Link to={`outbuilding`}>
-                      <ButtonB bsStyle="info" fill pullRight style={{ marginLeft: '10px' }}>
-                        <span className="fa fa-plus"></span>
-                        {' '}Nova dependência
-                        </ButtonB>
-                    </Link>
-                  )
-                }                
-              }}
-            </UserAuthContext.Consumer>
-          </Col>
-
-        </Row>
-
-        <Row style={{ paddingTop: '0px' }}>
-          <Col md={12}>
-            <Card
-              title="Lista de Dependências"
-              ctAllIcons
-              content={
-                <div>
-                  <Row>
-                    <Table striped hover>
-                      <thead>
-                        <tr>
-                          <th>Nome</th>
-                          <th>Disponibilidade</th>
-                          <th>Limite de pessoas</th>
-                          <th>Localização</th>
-                          <th className="text-center">Operações</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-
-
-                        <tr>
-                          <td>
-                            Salão de Jogos
-                          </td>
-                          <td>
-                            Segunda a Sexta - 14:00 ás 20:00
-                          </td>
-                          <td>
-                            20
-                          </td>
-                          <td>
-                            Bloco A
-                          </td>
-
-                          <td className="text-center" width={90}>
-                            {
-                              <div style={{ marginTop: '-10px', marginBottom: '-10px' }}>
-                                <UserAuthContext.Consumer>
-                                  {user => {
-                                    if (user.isManager) {
-                                      return <React.Fragment>
-                                        <Link to="outbuilding/teste">
-                                          <ButtonB 
-                                            bsStyle="success" bsSize="xs"
-                                            simple type="button" style={{ padding: '3px' }}>
-                                            <span className="fa fa-pencil"></span>
-                                          </ButtonB>
-                                        </Link>
-
-                                        {' '}
-                                        <ButtonB bsStyle="danger" simple type="button" bsSize="xs" style={{ padding: '3px' }}
-
-                                          onClick={this.excluirProduto}>
-                                          <span className="fa fa-times"></span>
-                                        </ButtonB>
-                                      </React.Fragment>;
-                                    }
-
-                                    return (
-                                      <Link to="outbuilding/teste">
-                                        <ButtonB 
-                                          bsStyle="success" bsSize="xs"
-                                          simple type="button" style={{ padding: '3px' }}>
-                                          <span className="fa fa-eye"></span>
-                                        </ButtonB>
-                                      </Link>
-                                    )
-                                  }}
-                                </UserAuthContext.Consumer>
-                              </div>
-                            }
-                          </td>
-                        </tr>
-
-                      </tbody>
-                    </Table>
-                  </Row>
-                </div>
-              }
-            />
-          </Col>
-        </Row>
-
-        <ButtonB
-          //disabled={this.state.disabledButtons || this.state.disabledButtonAnterior}
-          onClick={this.paginaAnterior}>
-          <span className="fa fa-caret-left"></span>
-            Anterior
-          </ButtonB>
-        {' '}
-        <ButtonB
-          //disabled={this.state.disabledButtons || this.state.disabledButtonProximo}
-          onClick={this.paginaProxima}>
-          Próximo
-          <span className="fa fa-caret-right"></span>
-        </ButtonB>
-
-        {/**confirma exclusao */}
-        <Modal
-          show={false}
-          aria-labelledby="contained-modal-title">
-          <Modal.Header closeButton={true}
-            onHide={this.cancelaExcluirProduto}>
-            <Modal.Title>Excluir Produto</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>Confirma a exclusão do produto selecionado?</Modal.Body>
-
-          <Modal.Footer>
-            <ButtonB fill bsStyle="danger" onClick={this.confirmaExcluirProduto}>Excluir</ButtonB>
-            <ButtonB fill onClick={this.cancelaExcluirProduto}>Cancelar</ButtonB>
-          </Modal.Footer>
-        </Modal>
-
-
-      </div>
-    );
-  }
+const searchOne = (query, history) => {
+  console.log(`Procurando ${query} ...`);
 }
+
+const onView = (outb, history) => {
+  history.push(`ticket/${outb._id}`);
+}
+
+const Operations = ({ item, selectItem }) => (
+  <div style={{ marginTop: '-10px', marginBottom: '-10px' }}>
+    <UserAuthContext.Consumer>
+      {user => {
+        const link = `outbuilding/${item._id}`;
+
+        if (user.isManager) {
+          return <React.Fragment>
+            <Link to={link}>
+              <ButtonB 
+                bsStyle="success" bsSize="xs"
+                simple type="button" style={{ padding: '3px' }}>
+                <span className="fa fa-pencil"></span>
+              </ButtonB>
+            </Link>
+
+            {' '}
+            <ButtonB bsStyle="danger" simple type="button" bsSize="xs" style={{ padding: '3px' }}
+              onClick={selectItem}>
+              <span className="fa fa-times"></span>
+            </ButtonB>
+          </React.Fragment>;
+        }
+
+        return (
+          <Link to={link}>
+            <ButtonB 
+              bsStyle="success" bsSize="xs"
+              simple type="button" style={{ padding: '3px' }}>
+              <span className="fa fa-eye"></span>
+            </ButtonB>
+          </Link>
+        )
+      }}
+    </UserAuthContext.Consumer>
+  </div>
+)
+
+const OutbuildingList = ({}) => (
+  <UserAuthContext.Consumer>
+    {
+      user => <Listing 
+                name="dependência" 
+                title="Lista de dependências"
+                headers={Headers}
+                dataReducer={dataReducer}
+                searchOne={searchOne} 
+                onItemClick={onView}
+                onItemRemoval={OutbuildingDomain.remove}
+                fetchItems={OutbuildingDomain.list} 
+                OperationsComponent={Operations} 
+                addLink={user.isManager && `outbuilding`}/>
+    }
+  </UserAuthContext.Consumer>
+)
 
 export default OutbuildingList;
