84c73e3a950b9a1cfd273b5c1f343dfa2ff7a8d2
= <gustavosm994@gmail.com>
2020-11-26 22:42:57 -0300

    Refactoring shedule list

--- src/views/schedule/ScheduleList.jsx
+++ src/views/schedule/ScheduleList.jsx
@@ -1,164 +1,89 @@
-import React, { Component } from "react";
-
-import { Modal, Table, FormControl } from "react-bootstrap";
-import { Row, Col } from "reactstrap";
-
-import Card from "components/Card/Card";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import ButtonB from "components/CustomButton/CustomButton.jsx";
-
-import api from '../../services/api';
-import { Link } from "react-router-dom";
 import { UserAuthContext } from "contexts";
+import { Listing } from "components/Listing";
+import ScheduleDomain from "domains/schedule";
 
+const dataReducer = shedule => [
+    shedule.resident,
+    shedule.dependency,
+    shedule.date,
+    shedule.hour,
+    shedule.NumOccupants,
+]
 
-class ScheduleList extends Component {
-    static contextType = UserAuthContext;
+const Headers = [
+    "Morador",
+    "dependência",
+    "data",
+    "horário",
+    "QTD. de ocupantes"
+]
 
-    constructor(props) {
-        super(props);
+const searchOne = (query, history) => {
+    console.log(`Procurando ${query} ...`);
+}
 
-        this.state = {
-            isHidden: false,
-            disableButtons: true,
-            modalExcluir: false,
-        };
-    };
+const onView = (outb, history) => {
+    history.push(ScheduleDomain.itemPath(outb));
+}
 
-    render() {
-        return (
-            <div className="content">
+const Operations = ({ item, selectItem }) => (
+    <div style={{ marginTop: '-10px', marginBottom: '-10px' }}>
+        <UserAuthContext.Consumer>
+            {user => {
+                const link = ScheduleDomain.itemPath(item);
 
-                <Row style={{ marginTop: '-20px' }}>
-
-                    <Col md={4} style={{ marginBottom: '5px', paddingRight: 10 }}>
-                        <FormControl
-                            style={{ marginBottom: '15px' }}
-                            type="text"
-                            //value={this.state.inputLocalizar}
-                            onChange={(e) => { this.setState({ inputLocalizar: e.target.value }) }}
-                            onKeyUp={(valor) => valor.key == 'Enter' ? this.listarTransportadoras() : ''}
-                            placeholder="Localizar Agendamento"
-                        />
-                    </Col>
-
-                    <Col md={8} style={{ marginBottom: '5px', paddingLeft: 0 }}>
-                        <ButtonB
-                            bsStyle="info"
-                            //disabled={this.state.disabledButtons}
-                            onClick={this.listarTransportadoras}>
-                            <span className="fa fa-search"></span>Localizar
-                        </ButtonB>
-
-                        <Link to={`schedule`}>
-                            <ButtonB bsStyle="info" fill pullRight style={{ marginLeft: '10px' }}>
-                                <span className="fa fa-plus"></span>
+                if (user.isManager) {
+                    return <React.Fragment>
+                        <Link to={link}>
+                            <ButtonB
+                                bsStyle="success" bsSize="xs"
+                                simple type="button" style={{ padding: '3px' }}>
+                                <span className="fa fa-pencil"></span>
                             </ButtonB>
                         </Link>
-                    </Col>
-                </Row>
 
-                <Row style={{ paddingTop: '0px' }}>
-                    <Col md={12}>
-                        <Card
-                            title="Lista de Agendamentos"
-                            ctAllIcons
-                            content={
-                                <div>
-                                    <Row>
-                                        <Table striped hover>
-                                            <thead>
-                                                <tr>
-                                                    {this.context.isManager &&
-                                                        <th>Morador</th>
-                                                    }
-                                                    <th>Dependência</th>
-                                                    <th>
-                                                        Data
-                                                    </th>
-                                                    <th>
-                                                        Horário
-                                                    </th>
-                                                    <th>
-                                                        Qtd. de ocupantes
-                                                    </th>
-                                                    <th className="text-center">Operações</th>
-                                                </tr></thead>
-                                            <tbody>
-                                                <tr>
-                                                    {this.context.isManager &&
-                                                        <td>Pelé</td>
-                                                    }
-                                                    <td>Piscina</td>
-                                                    <td>19/10/2020</td>
-                                                    <td>14:30 ás 15:30</td>
-                                                    <td>Cheio</td>
-                                                    <td className="text-center" width={140}>
-                                                        {
-                                                            <div style={{ marginTop: '-10px', marginBottom: '-10px' }}>
+                        {' '}
+                        <ButtonB bsStyle="danger" simple type="button" bsSize="xs" style={{ padding: '3px' }}
+                            onClick={selectItem}>
+                            <span className="fa fa-times"></span>
+                        </ButtonB>
+                    </React.Fragment>;
+                }
 
-                                                                <Link to={`schedule/teste`}>
-                                                                    <ButtonB bsStyle="success" simple type="button" 
-                                                                        bsSize="xs" style={{ padding: '3px' }} >
-                                                                        <span className="fa fa-eye"></span>
-                                                                    </ButtonB>
-                                                                </Link>
+                return (
+                    <Link to={link}>
+                        <ButtonB
+                            bsStyle="success" bsSize="xs"
+                            simple type="button" style={{ padding: '3px' }}>
+                            <span className="fa fa-eye"></span>
+                        </ButtonB>
+                    </Link>
+                )
+            }}
+        </UserAuthContext.Consumer>
+    </div>
+)
 
-                                                                {' '}
-                                                                <ButtonB bsStyle="danger" simple type="button" bsSize="xs" style={{ padding: '3px' }}
-
-                                                                    onClick={this.excluirTransportadora}>
-                                                                    <span className="fa fa-times"></span>
-                                                                </ButtonB>
-                                                                {' '}
-                                                            </div>
-                                                        }
-                                                    </td>
-                                                </tr>
-
-                                            </tbody>
-                                        </Table>
-                                    </Row>
-                                </div>
-                            }
-                        />
-                    </Col>
-                </Row>
-
-                <ButtonB
-                    //disabled={this.state.disabledButtons || this.state.disabledButtonAnterior}
-                    onClick={this.paginaAnterior}>
-                    Anterior
-                    <span className="fa fa-caret-left"></span>
-                </ButtonB>
-                {' '}
-                <ButtonB
-                    //disabled={this.state.disabledButtons || this.state.disabledButtonProximo}
-                    onClick={this.paginaProxima}>
-                    Próximo
-                    <span className="fa fa-caret-right"></span>
-                </ButtonB>
-
-                {/**confirma exclusao */}
-                <Modal
-                    show={this.state.modalExcluir}
-                    aria-labelledby="contained-modal-title">
-                    <Modal.Header closeButton={true}
-                        onHide={this.cancelaexcluirTransportadora}>
-                        <Modal.Title>Excluir Agendamento</Modal.Title>
-                    </Modal.Header>
-
-                    <Modal.Body>Confirma a exclusão da transportadora selecionada ?</Modal.Body>
-
-                    <Modal.Footer>
-                        <ButtonB fill bsStyle="danger" onClick={this.confirmaexcluirTransportadora}>Excluir</ButtonB>
-                        <ButtonB fill onClick={this.cancelaexcluirTransportadora}>Cancelar</ButtonB>
-                    </Modal.Footer>
-                </Modal>
-
-            </div>
-        )
-    }
-}
+const ScheduleList = ({ }) => (
+    <UserAuthContext.Consumer>
+        {
+            user => <Listing
+                name="agendamentos"
+                title="Lista de Agendamentos"
+                headers={Headers} www
+                dataReducer={dataReducer}
+                searchOne={searchOne}
+                onItemClick={onView}
+                onItemRemoval={ScheduleDomain.remove}
+                fetchItems={ScheduleDomain.list}
+                OperationsComponent={Operations}
+                addLink={user.isManager && ScheduleDomain.resource} />
+        }
+    </UserAuthContext.Consumer>
+)
 
 export default ScheduleList;
