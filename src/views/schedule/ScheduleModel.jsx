edd07ab9a72c85d7d7a9629bb5344c64fd31f6c9
= <gustavosm994@gmail.com>
2020-12-01 02:04:39 -0300

    Refactoring model schedule

--- src/views/schedule/ScheduleModel.jsx
+++ src/views/schedule/ScheduleModel.jsx
@@ -15,6 +15,7 @@ import { ModelComponent } from "views/common";
 import { UserAuthContext } from "contexts";
 import { TableComponent } from "components/Listing";
 import ButtonB from "components/CustomButton/CustomButton.jsx";
+import ScheduleDomain from "domains/schedule";
 
 const scheduleHeaders = [
     "Morador",
@@ -45,11 +46,34 @@ class ScheduleModel extends ModelComponent {
             hasCollision: false,
         }
 
+        this.domain = ScheduleDomain;
+
         this.collisionedSchedule = null;
     }
 
-    // TODO use a better method to test which operation this model is in
-    isViewing = () => this.state.outbuildingId !== undefined
+    mapData = () => {
+        const {
+            resident,
+            outbuilding,
+            date,
+            schedule,
+            NumOccupants
+        } = this.state;
+
+        return {
+            resident,
+            outbuilding,
+            date,
+            schedule,
+            NumOccupants
+        }
+    }
+
+    tabs = () => {
+        return this.store()
+    }
+
+    disableFields = () => this.isEditing;
 
     handleSubmit = e => {
         this.collisionedSchedule = {
@@ -67,172 +91,125 @@ class ScheduleModel extends ModelComponent {
         this.setState({ hasCollision: true })
     }
 
-    render() {
-        const styleInput = {
-            fontSize: 12,
-            height: '38px'
-        }
+    store = () => (
 
-        const styleInputUpper = {
-            fontSize: 12,
-            textTransform: 'uppercase'
-        }
+        <React.Fragment>
+            <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
+                {
+                    this.context.isManager && this.state.resident &&
+                    <Col md={3}>
+                        <AvGroup>
+                            <Label for="residentName" >Morador</Label>
+                            <AvField
+                                type="text"
+                                name="residentName"
+                                id="residentName"
 
-        const styleLabel = {
-            fontSize: 11
-        }
-        return (
-            <div className="content">
-                <Row>
-                    <Col md={12}>
-
-                        <Card>
-                            <CardBody>
-
-                                <AvForm autoComplete="off" onSubmit={this.operacaoTransportadora} ref="formSchedule">
-                                    <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
-                                        {this.context.isManager && this.state.resident &&
-                                            <Col md={3}>
-                                                <AvGroup>
-                                                    <Label for="residentName" style={styleLabel}>Morador</Label>
-                                                    <AvField 
-                                                        type="text" 
-                                                        name="residentName" 
-                                                        id="residentName" 
-                                                        style={styleInput}
-                                                        disabled={this.isViewing()}
-                                                        value={this.state.resident.name} />
-                                                </AvGroup>
-                                            </Col>
-                                        }
-
-                                        <Col md={3}>
-                                            <AvGroup>
-                                                <Label for="outbuildingId" style={styleLabel}>Dependência</Label>
-                                                <AvField type="select" name="outbuildingId" id="outbuildingId" style={styleInput}
-                                                    value={this.getModelAttr('outbuildingId')}
-                                                    disabled={this.isViewing()}
-                                                    validate={{
-                                                        required: { value: true, errorMessage: 'Campo "Nome" obrigatório' },
-                                                    }} >
-                                                    <option value="0">Salão de jogos - Bloco B</option>
-                                                    <option value="1">Piscina</option>
-                                                </AvField>
-                                            </AvGroup>
-                                        </Col>
-                                        <Col md={6} >
-                                            <AvGroup>                                                
-                                                <DayPicker 
-                                                    selectedDays={this.getModelAttr('day')}
-                                                    onDayClick={this.isViewing() ? null : this.setModelAttrValue('day')} />
-                                            </AvGroup>
-                                        </Col>
-                                    </Row>
-                                    <Row style={{ paddingLeft: '10px', paddingRight: '10px' }} >
-                                        <Col style={{ marginTop: '-10px' }} md={2}>
-                                            <AvGroup>
-                                                <Label for="fromHour" style={styleLabel}>Hora início</Label>
-                                                <AvField type="time" name="fromHour" id="fromHour" style={styleInput}
-                                                    disabled={this.isViewing()}
-                                                    validate={{
-                                                        required: { value: true, errorMessage: 'Campo "Hora início" obrigatório' },
-                                                    }} />
-                                            </AvGroup>
-                                        </Col>
-                                        <Col style={{ marginTop: '-10px' }} md={2}>
-                                            <AvGroup>
-                                                <Label for="toHour" style={styleLabel}>Hora fim</Label>
-                                                <AvField type="time" name="toHour" id="toHour" style={styleInput}
-                                                    disabled={this.isViewing()}
-                                                    validate={{
-                                                        required: { value: true, errorMessage: 'Campo "Hora fim" obrigatório' },
-                                                    }} />
-                                            </AvGroup>
-                                        </Col>
-                                        <Col md={3} style={{ marginTop: '-10px' }}>
-                                            <AvGroup>
-                                                <Label for="peopleCount" style={styleLabel}>Quantidade de ocupantes</Label>
-                                                <AvField type="number" name="peopleCount" id="peopleCount" 
-                                                    min={1}
-                                                    style={styleInput}
-                                                    disabled={this.isViewing()}
-                                                    value={this.getModelAttr('peopleCount')} 
-                                                    placeholder="Ocupação total">
-                                                </AvField>
-                                            </AvGroup>
-                                        </Col>
-                                    </Row>
-
-                                    <Row style={{ paddingLeft: '10px', paddingRight: '10px' }} >
-                                        <Col md={12} style={{ marginTop: '-10px' }}>
-                                            <AvGroup>
-                                                <Label for="notes" style={styleLabel}>Observações</Label>
-                                                <AvField type="textarea" rows={5} name="notes" id="notes"
-                                                    disabled={this.isViewing()}
-                                                    value={this.getModelAttr('notes')} />
-                                            </AvGroup>
-                                        </Col>
-                                    </Row>
-                                </AvForm>
-                                <div className="clearfix" />
-                            </CardBody>
-                        </Card>
-                        <AvForm autoComplete="off" onSubmit={this.operacaoTransportadora} ref="formSchedule">
-                            <Button pullRight fill bsStyle="danger"
-                                onClick={this.retornarListaTransportadora}>
-                                Cancelar
-                            </Button>
-
-                            <Button 
-                                bsStyle="success" 
-                                fill 
-                                type="submit"
-                                onClick={this.handleSubmit} >
-                                Gravar
-                            </Button>
-                        </AvForm>
+                                value={this.state.resident.name} />
+                        </AvGroup>
                     </Col>
-                </Row>
+                }
 
-                {/**confirma exclusao */}
-                <Modal
-                    show={this.state.hasCollision}
-                    aria-labelledby="contained-modal-title">
-                    <Modal.Header>
-                        <Modal.Title>Conflito</Modal.Title>
-                    </Modal.Header>
+                <Col md={3}>
+                    <AvGroup>
+                        <Label for="outbuildingId" >Dependência</Label>
+                        <AvField type="select" name="outbuildingId" id="outbuildingId"
+                            value={this.getModelAttr('outbuildingId')}
+                            validate={{
+                                required: { value: true, errorMessage: 'Campo "Nome" obrigatório' },
+                            }} >
+                            <option value="0">Salão de jogos - Bloco B</option>
+                            <option value="1">Piscina</option>
+                        </AvField>
+                    </AvGroup>
+                </Col>
+                <Col md={6} >
+                    <AvGroup>
+                        <DayPicker
+                            selectedDays={this.getModelAttr('day')}
+                        //onDayClick={this.isViewing() ? null : this.setModelAttrValue('day')}
+                        />
+                    </AvGroup>
+                </Col>
+            </Row>
+            <Row style={{ paddingLeft: '10px', paddingRight: '10px' }} >
+                <Col style={{ marginTop: '-10px' }} md={2}>
+                    <AvGroup>
+                        <Label for="fromHour" >Hora início</Label>
+                        <AvField type="time" name="fromHour" id="fromHour"
+                            validate={{
+                                required: { value: true, errorMessage: 'Campo "Hora início" obrigatório' },
+                            }} />
+                    </AvGroup>
+                </Col>
+                <Col style={{ marginTop: '-10px' }} md={2}>
+                    <AvGroup>
+                        <Label for="toHour">Hora fim</Label>
+                        <AvField type="time" name="toHour" id="toHour"
+                            validate={{
+                                required: { value: true, errorMessage: 'Campo "Hora fim" obrigatório' },
+                            }} />
+                    </AvGroup>
+                </Col>
+                <Col md={3} style={{ marginTop: '-10px' }}>
+                    <AvGroup>
+                        <Label for="peopleCount">Quantidade de ocupantes</Label>
+                        <AvField type="number" name="peopleCount" id="peopleCount"
+                            min={1}
+                            value={this.getModelAttr('peopleCount')}
+                            placeholder="Ocupação total">
+                        </AvField>
+                    </AvGroup>
+                </Col>
+            </Row>
 
-                    <Modal.Body>
-                        <p className="text-center">
-                            Lamentamos informar mas o agendamento solicitado com um já existente.
-                            Um ticket será aberto.
-                        </p>
-                        <TableComponent 
-                            headers={scheduleHeaders}
-                            items={[this.collisionedSchedule]}
-                            dataReducer={scheduleDataReducer} />
-                    </Modal.Body>
+            <Row style={{ paddingLeft: '10px', paddingRight: '10px' }} >
+                <Col md={12} style={{ marginTop: '-10px' }}>
+                    <AvGroup>
+                        <Label for="notes">Observações</Label>
+                        <AvField type="textarea" rows={5} name="notes" id="notes"
+                            value={this.getModelAttr('notes')} />
+                    </AvGroup>
+                </Col>
+            </Row>
 
-                    <Modal.Footer>
-                        <Row style={{ paddingLeft: '10px', paddingRight: '10px' }} >
-                            <Col md={2}>
-                                <Link to={"ticket/teste1"}>
-                                    <ButtonB pullLeft fill bsStyle="info">Ver ticket</ButtonB>
-                                </Link>
-                            </Col>
-                            
-                            <Col md={10}>
-                                <Link to={"schedule/teste1"}>
-                                    <ButtonB fill>Entendi</ButtonB>
-                                </Link>
-                            </Col>
-                        </Row>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        )
-    }
+            < Modal
+                show={this.state.hasCollision}
+                aria-labelledby="contained-modal-title" >
+                <Modal.Header>
+                    <Modal.Title>Conflito</Modal.Title>
+                </Modal.Header>
 
+                <Modal.Body>
+                    <p className="text-center">
+                        Lamentamos informar mas o agendamento solicitado com um já existente.
+                        Um ticket será aberto.
+                    </p>
+                    <TableComponent
+                        headers={scheduleHeaders}
+                        items={[this.collisionedSchedule]}
+                        dataReducer={scheduleDataReducer} />
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Row style={{ paddingLeft: '10px', paddingRight: '10px' }} >
+                        <Col md={2}>
+                            <Link to={"ticket/teste1"}>
+                                <ButtonB pullLeft fill bsStyle="info">Ver ticket</ButtonB>
+                            </Link>
+                        </Col>
+
+                        <Col md={10}>
+                            <Link to={"schedule/teste1"}>
+                                <ButtonB fill>Entendi</ButtonB>
+                            </Link>
+                        </Col>
+                    </Row>
+                </Modal.Footer>
+            </Modal >
+
+        </React.Fragment>
+    )
 }
 
 export default ScheduleModel;
