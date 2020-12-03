5aa3f203222633a8258fb192ea151522dfef5bac
Yan Marques de Cerqueira <yan.cerqueira@unisul.br>
2020-12-03 18:43:33 -0300

    Refactor outbuilding model to use virtual fields

--- src/views/outbuilding/OutbuildingModel.jsx
+++ src/views/outbuilding/OutbuildingModel.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { AvField, AvGroup } from 'availity-reactstrap-validation';
-import {
-  Row, Col, Label, Table
-} from "reactstrap";
 
 import Button from "components/CustomButton/CustomButton";
-import { TableComponent } from "components/Listing";
 import { ModelComponent } from "views/common";
-import { UserAuthContext } from "contexts";
 import OutbDomain from "domains/outbuilding";
 
 const DAYS = [
@@ -21,49 +15,72 @@ const DAYS = [
 ]
 
 class OutbuildingModel extends ModelComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      _tmpAvailabilities: [],
-      _tmpMedias: [],
-      ...this.state,
-    }
+  domain = OutbDomain;
 
-    this.domain = OutbDomain;
-  }
+  disableFields = () => !this.context.isManager
 
-  mapData = () => {
+  mapData = values => {
     const {
       name,
       description,
       capacity,
-      availabilities,
-      medias,
-    } = this.state;
+    } = values;
 
     return {
       name,
       description,
       capacity,
-      availabilities,
-      medias,
     }
   }
 
-  tabs = () => [
-    {
-      name: 'Cadastro',
-      component: this.storeTab() 
-    },
-    {
-      name: 'Horários disponíveis',
-      component: this.availabilityTab()
-    },
-    {
-      name: 'Mídia',
-      component: this.mediaTab()
-    }
+  tabAndFields = () => [
+    this.storeTab(),
+    this.availabilityTab(),
+    this.mediaTab(),
   ]
+
+  storeTab = () => {
+    return {
+      name: "Cadastro",
+      rows: [
+        {
+          name: {
+            type: "text",
+            label: "Nome",
+          },
+          condominiumId: {
+            type: "select",
+            label: "Condomínio",
+            required: false,
+            children: <React.Fragment>
+              <option>Selecionar</option>
+              <option value="0">São joao</option>
+            </React.Fragment>
+          },
+          capacity: {
+            type: "number",
+            label: "Lotação máxima",
+            helpMessage: "Número máximo de pessoas no ambiente",
+            min: 1,
+            col: {
+              xs: 4,
+            }
+          },
+        },
+        {
+          description: {
+            type: "textarea",
+            label: "Descrição",
+            col: {
+              xs: 16,
+              md: 12,
+              sm: 8,
+            }
+          },
+        }
+      ]
+    }
+  }
 
   selectDayComponent = props => (
     <React.Fragment>
@@ -74,99 +91,7 @@ class OutbuildingModel extends ModelComponent {
     </React.Fragment>
   ) 
 
-  fieldProps = () => {
-    return {
-      name: {
-        type: "text",
-        label: "Nome",
-      },
-      description: {
-        type: "textarea",
-        label: "Descrição",
-      },
-      capacity: {
-        type: "number",
-        label: "Lotação máxima",
-      },
-      condominiumId: {
-        type: "select",
-        label: "Condomínio",
-      },
-      fromDay: {
-        type: "select",
-        children: this.selectDayComponent(),
-      },
-      toDay: {
-        type: "select",
-        children: this.selectDayComponent(),
-      },
-      fromHour: {
-        type: "time",
-      },
-      toHour: {
-        type: "time",
-      },
-      selectedMedia: {
-        type: "file",
-        multiple: true,
-        onChange: e => {
-          const newMedias = this.state._tmpMedias.concat(...e.target.files);
-          this.setState({ _tmpMedias: newMedias })
-        } 
-      }
-    }
-  }
-
-  disableFields = () => !this.context.isManager
-
-  removeObjFrom = (key, item) => {
-    return () => {
-      try {
-        const index = this.state[key].indexOf(item);
-        delete this.state[key][index];
-        this.setState({ [key]: this.state[key] })
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  }
-
-  storeTab = () => (
-    <React.Fragment>
-      <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
-        <Col style={{ marginTop: '-10px' }} md={4}>
-          <AvGroup>
-            {this.field("name")}
-          </AvGroup>
-        </Col>
-        <Col style={{ marginTop: '-10px' }} md={4}>
-          <AvGroup>
-            {this.field("condominiumId")}
-          </AvGroup>
-        </Col>
-      </Row>
-      <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
-        <Col style={{ marginTop: '-10px' }} md={2}>
-          <AvGroup>
-            {this.field("capacity")}
-          </AvGroup>
-        </Col>
-
-        {/* <Col style={{ marginTop: '-10px' }} md={4}>
-          <AvGroup>
-            <Label for="location" >Localização</Label>
-            <AvField type="text" name="location" id="location" 
-              onChange={this.setModelAttr('location')}
-              disabled={isDisabled}
-              value={this.state.outbuilding.location}
-              placeholder="Ex.: Bloco B" />
-          </AvGroup>
-        </Col> */}
-      </Row>
-    </React.Fragment>
-  )
-
-  availabilityTab = isDisabled => {
+  availabilityTab = () => {
     const headers = ["Dia(s) da semana", "Das", "Até"]
 
     const dataReducer = availability => [
@@ -175,73 +100,72 @@ class OutbuildingModel extends ModelComponent {
       availability.toHour,
     ]
 
-    const operations = props => (
-      <div style={{ marginTop: '-10px', marginBottom: '-10px' }}>
-        <Button bsStyle="danger" simple type="button" bsSize="xs" style={{ padding: '3px' }}
-          onClick={this.removeObjFrom('_tmpAvailabilities', props.item)}>
-          <span className="fa fa-times"></span>
-        </Button>
-      </div>
-    )
+    const mapValues = values => {
+      const {
+        fromDay,
+        toDay,
+        fromHour,
+        toHour,
+      } = values;
 
-    return <React.Fragment>
-      {this.context.isManager &&
-        <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
-          <Col md={2} style={{ marginTop: '-10px' }}>
-            <AvGroup>
-              {this.field("fromDay")}
-            </AvGroup>
-          </Col>
-          <Col md={2} style={{ marginTop: '-10px' }}>
-            <AvGroup>
-              {this.field("toDay")}
-            </AvGroup>
-          </Col>
-          <Col style={{ marginTop: '-10px' }} md={2}>
-            <AvGroup>
-              {this.field("fromHour")}
-            </AvGroup>
-          </Col>
-          <Col style={{ marginTop: '-10px' }} md={2}>
-            <AvGroup>
-              {this.field("toHour")}
-            </AvGroup>
-          </Col>
-
-          <Button bsStyle="info" fill
-            style={{ margin: '15px' }}
-            onClick={() => {
-              const {
-                fromDay,
-                toDay,
-                fromHour,
-                toHour,
-              } = this.state;
-
-              const availability = {
-                fromDay,
-                toDay,
-                fromHour,
-                toHour,
-              }
-              this.state._tmpAvailabilities.push(availability)
-              this.setState({ _tmpAvailabilities: this.state._tmpAvailabilities })
-            }}>
-            <span className="fa fa-plus"></span>
-            {' '}Adicionar
-          </Button>
-        </Row>
+      return {
+        fromDay,
+        toDay,
+        fromHour,
+        toHour,
       }
-      <Row style={{ paddingLeft: '15px', paddingRight: '15px' }}>
-        <Col style={{ marginTop: '-10px' }} md={8}>
-          <TableComponent
-              items={this.state._tmpAvailabilities.concat(...this.state.availabilities || [])}
-              headers={headers}
-              dataReducer={dataReducer}
-              OperationsComponent={this.context.isManager && operations} />
-        </Col>
-      </Row>
-    </React.Fragment>
+    }
+
+    const col = {
+      md: 2,
+      sm: 6,
+    }
+
+    const tabCfg = {
+      fromDay: {
+        type: "select",
+        children: this.selectDayComponent(),
+        col
+      },
+      toDay: {
+        type: "select",
+        required: false,
+        children: this.selectDayComponent(),
+        col
+      },
+      fromHour: {
+        type: "time",
+        col
+      },
+      toHour: {
+        type: "time",
+        col
+      },
+      addbutton: {
+        as: Button,
+        required: false,
+        bsStyle: "info",
+        type: "submit",
+        fill: true,
+        children: <React.Fragment>
+           <span className="fa fa-plus"></span>
+            Adicionar
+        </React.Fragment>,
+      }
+    }
+
+    return {
+      name: "Horários disponíveis",
+      provides: "availabilities",
+      isVirtual: true,
+      validate: values => values.length != 0,
+      rows: [
+        this.context.isManager && tabCfg
+      ],
+      mapValues,
+      headers,
+      dataReducer,
+    }
   }
 
   mediaTab = () => {
@@ -250,45 +174,44 @@ class OutbuildingModel extends ModelComponent {
       media.name
     ]
 
-    const operations = props => this.context.isManager &&
-      <React.Fragment>
-        {' '}
-        <Button bsStyle="danger" simple type="button" bsSize="xs"
-            onClick={this.removeObjFrom('_tmpMedias', props.item)}>
-          <span className="fa fa-times"></span>
-        </Button>
-      </React.Fragment> || 
-      <React.Fragment>
-        {' '}
-        <Button bsStyle="success" simple type="button" bsSize="xs">
-          <span className="fa fa-eye"></span>
-        </Button>
-      </React.Fragment> 
+    const tabCfg = {
+      selectedMedia: {
+        type: "file",
+        multiple: true,
+        required: false,
+        onChange: async(e) => {
+          // get current form reference
+          const currentForm = this.currentForm();
 
-    return <React.Fragment>
-      {this.context.isManager &&
-        <Row style={{ paddingTop: '10px', paddingLeft: '10px', paddingRight: '10px' }}>
-          <Col md={4} style={{ marginTop: '-10px' }}>
-            <AvGroup>
-              {this.field("selectedMedia")}
-            </AvGroup>
-          </Col>
-        </Row>
+          // get a copy of the FileList
+          const files = e.target.files;
+
+          // patch input with files
+          currentForm._inputs.selectedMedia.getValue = () => files;
+
+          // submit form
+          await currentForm.submit();
+        }
       }
-      <Row style={{ paddingLeft: '15px', paddingRight: '10px' }}>
-        <Col style={{ marginTop: '-10px' }} md={8}>
-          <TableComponent 
-            items={this.state._tmpMedias.concat(...this.state.medias || [])}
-            headers={headers}
-            dataReducer={dataReducer}
-            OperationsComponent={operations}
-            />
-        </Col>
-      </Row>
-    </React.Fragment>
+    }
+
+    const mapValues = values => {
+      return [...values.selectedMedia];
+    }
+
+    return {
+      name: "Mídias",
+      provides: "medias",
+      isVirtual: true,
+      validate: values => true,
+      rows: [
+        this.context.isManager && tabCfg
+      ],
+      mapValues,
+      headers,
+      dataReducer,
+    }
   }
 }
-
-OutbuildingModel.contextType = UserAuthContext;
 
 export default OutbuildingModel;
